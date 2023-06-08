dc6f6dddb58b6f156394a73726b8f62acc85f305
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-08 10:57:35 +0900

    [Bronze III] Title: 영수증, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum;
+	int a, b, c, d, e, f, g, h, i;
+	int value;
+	cin >> sum >> a >> b >> c >> d >> e >> f >> g >> h >> i;
+	value = sum - (a + b + c + d + e + f + g + h + i);
+	cout << value;
+}
