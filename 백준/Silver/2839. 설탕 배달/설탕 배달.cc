417a63bd57180be39133edeb1fc9848922f7cca3
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-15 15:42:56 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int n, b;
+	cin >> n;
+	int a = n / 5;
+	while (1)
+	{
+		if (a < 0)
+		{
+			cout << "-1";
+			return 0;
+		}
+		if ((n - (5 * a)) % 3 == 0)
+		{
+			b = (n - (5 * a)) / 3;
+			break;
+		}
+		a--;
+	}
+	cout << a + b;
+}
