3748b023344ae40ee00749d24327bd089bc3ffec
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-19 12:43:18 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.01 ms, Memory: 3.74 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string myString) {
+    for (int i=0;i<myString.length();i++)
+    {
+        if (myString[i]>='a'&&myString[i]<='z')
+            myString[i]=myString[i]-32;
+    }
+    return myString;
+}
