edcb00335efbffe30c6d01206bcf789a3bad5875
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-23 16:09:32 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.05 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120894. 영어가 싫어요/영어가 싫어요.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <regex>
+using namespace std;
+
+long long solution(string numbers) {
+    long long answer = 0;
+    numbers=regex_replace(numbers,regex("zero"),"0");
+    numbers=regex_replace(numbers,regex("one"),"1");
+    numbers=regex_replace(numbers,regex("two"),"2");
+    numbers=regex_replace(numbers,regex("three"),"3");
+    numbers=regex_replace(numbers,regex("four"),"4");
+    numbers=regex_replace(numbers,regex("five"),"5");
+    numbers=regex_replace(numbers,regex("six"),"6");
+    numbers=regex_replace(numbers,regex("seven"),"7");
+    numbers=regex_replace(numbers,regex("eight"),"8");
+    numbers=regex_replace(numbers,regex("nine"),"9");
+    answer=stoll(numbers);
+    return answer;
+}
