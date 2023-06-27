5a7711ea63958506445e2bb60a16db0384c8c04e
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-27 13:39:41 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 124 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<int, int>a, pair<int, int>b)
+{
+	if (a.second == b.second)
+		return a.first < b.first;
+	else
+		return a.second < b.second;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	int x, y;
+	vector<pair<int, int>> v;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x >> y;
+		v.push_back({ x,y });
+	}
+	sort(v.begin(), v.end(),compare);
+	for (int i = 0; i < n; i++)
+	{
+		cout << v[i].first << " " << v[i].second << "\n";
+	}
+}
