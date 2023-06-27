ef379cfc803f09304bd3ccd695331d44f195e6af
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-27 15:58:09 +0900

    [level 0] Title: 숫자 찾기, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120904. 숫자 찾기/숫자 찾기.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num, int k) {
+    int answer = -1;
+    string s = to_string(num);
+    for (int i=0;i<s.length();i++)
+    {
+        if (s[i]-'0'==k)
+        {
+            answer=i+1;
+            break;
+        }
+        
+    }
+    return answer;
+}
