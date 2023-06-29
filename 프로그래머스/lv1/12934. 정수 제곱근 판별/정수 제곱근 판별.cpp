1099a01110335657c6ea6c4c56a5380dbff3ed23
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-29 15:51:11 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12934. 정수 제곱근 판별/정수 제곱근 판별.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    long long a=sqrt(n);
+    if (n==a*a)
+        answer=pow(a+1,2);
+    else
+        answer=-1;
+    return answer;
+}
