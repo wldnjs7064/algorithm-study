0991ed03891af460171080a23f72f52abd600c53
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-06-29 14:16:55 +0900

    [unrated] Title: 꼬리 문자열, Time: 0.02 ms, Memory: 3.71 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181841. 꼬리 문자열/꼬리 문자열.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> str_list, string ex) {
+    string answer = "";
+    for (const auto& str : str_list)
+    {
+        if (str.find(ex)==string::npos)
+            answer+=str;
+    }
+    return answer;
+}
