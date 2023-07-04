d58444afd6fe9b6ea13c442d691ec9dbb34fbcef
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-07-04 10:33:41 +0900

    [Silver V] Title: 숫자 카드, Time: 896 ms, Memory: 28252 KB -BaekjoonHub

---
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.cc
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+using namespace std;
+
+int main()
+{
+	int n, m;
+	int cnt = 0;
+	vector<int> v;
+	set<int> s;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		s.insert(x);
+	}
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		int y;
+		cin >> y;
+		v.push_back(y);
+	}
+	for (int i = 0; i < m; i++)
+	{
+		if (s.find(v[i]) != s.end())
+			cout << "1" << " ";
+		else
+			cout << "0" << " ";
+	}
+}
