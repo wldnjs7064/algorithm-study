09acbd84035ea8170b68dc99d57a33f8ed998fd6
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-08-21 14:04:59 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cc
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	while (true) {
+		stack<char> S;
+		string str;
+
+		getline(cin, str);
+
+		if (str == ".")
+			break;
+
+		bool flag = true;
+
+		for (int i = 0; i < str.size(); i++) {
+
+			if (str[i] == '[' || str[i] == '(') {
+				S.push(str[i]);
+			}
+
+			else if (str[i] == ']') {
+				if (S.empty()) {
+					flag = false;
+					break;
+				}
+
+				else if (S.top() == '[') {
+					S.pop();
+				}
+
+				else if (S.top() == '(') {
+					flag = false;
+					break;
+				}
+			}
+
+			else if (str[i] == ')') {
+				if (S.empty()) {
+					flag = false;
+					break;
+				}
+
+				else if (S.top() == '(') {
+					S.pop();
+				}
+
+				else if (S.top() == '[') {
+					flag = false;
+					break;
+				}
+			}
+
+		}
+
+		if (S.empty() && flag)
+			cout << "yes\n";
+
+		else
+			cout << "no\n";
+
+	}
+}
