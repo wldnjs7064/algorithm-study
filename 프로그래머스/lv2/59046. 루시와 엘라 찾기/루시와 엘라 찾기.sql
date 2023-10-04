7799cd38d2c23ca8c6aa283dc733d596579eb629
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-10-04 10:16:08 +0900

    [level 2] Title: 루시와 엘라 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
+++ 프로그래머스/lv2/59046. 루시와 엘라 찾기/루시와 엘라 찾기.sql
@@ -1,5 +1,5 @@
 -- 코드를 입력하세요
 SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
 FROM ANIMAL_INS
-WHERE NAME='Lucy' OR name='Ella' or name='Pickle' or name='Rogan' or name='Sabrina' or name='Mitty'
+WHERE NAME = 'Lucy' or NAME = 'Ella' or NAME = 'Pickle' or NAME = 'Rogan' or NAME = 'Sabrina' or NAME = 'Mitty'
 ORDER BY ANIMAL_ID;
