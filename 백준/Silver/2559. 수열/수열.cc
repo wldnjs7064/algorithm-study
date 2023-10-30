c8158ac2a8bed021a82a5988c02794eaff7f54f9
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-10-30 09:59:20 +0900

    [Silver III] Title: 수열, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/2559. 수열/수열.cc
+++ 백준/Silver/2559. 수열/수열.cc
@@ -1,28 +1,22 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 int main()
 {
-	int n, k, sum = 0, max;
+	int n, k, sum = 0 , max;
 	cin >> n >> k;
-	vector<int> v;
+	vector<int> v(n);
 	for (int i = 0; i < n; i++)
-	{
-		int x;
-		cin >> x;
-		v.push_back(x);
-	}
+		cin >> v[i];
 	for (int i = 0; i < k; i++)
-	{
 		sum += v[i];
-	}
 	max = sum;
 	for (int i = k; i < n; i++)
 	{
 		sum = sum + v[i] - v[i - k];
-		if (sum > max) max = sum;
+		if (sum > max)
+			max = sum;
 	}
 	cout << max;
 }
