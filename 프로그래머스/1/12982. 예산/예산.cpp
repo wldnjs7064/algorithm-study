e729b2ac6c9e37fe14b9804b4c6cdb5948895b2d
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-04 23:51:29 +0900

    [level 1] Title: 예산, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int solution(vector<int> d, int budget) {
+    int answer = 0;
+    sort(d.begin(),d.end());
+    int sum=0;
+    for (int i=0;i<d.size();i++)
+    {
+        sum+=d[i];
+        if (sum<=budget)
+            answer++;
+        else break;
+    }
+    return answer;
+}
