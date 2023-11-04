5a071b3dd5b95c5f030f3fbfa49861c868a4727a
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-05 00:08:42 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

---
+++ 프로그래머스/1/12930. 이상한 문자 만들기/이상한 문자 만들기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string s) {
+    int idx=0;
+    for (int i=0;i<s.length();i++)
+    {
+        if (s[i]==' ')
+        {
+            idx=0;
+            continue;
+        }
+        if (idx%2==0)
+        {
+            if (s[i]>='a'&&s[i]<='z')
+                s[i]-=32;
+        }
+        else
+            if (s[i]>='A'&&s[i]<='Z')
+                s[i]+=32;
+        idx++;
+    }
+    return s;
+}
