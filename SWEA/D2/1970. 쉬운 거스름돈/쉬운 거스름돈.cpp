bbb290823590338899a59d92395ae1cdb2b82972
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-16 23:48:37 +0900

    [D2] Title: 쉬운 거스름돈, Time: 6 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.cpp
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.cpp
@@ -1,23 +1,21 @@
-#include<iostream>
-
+#include <iostream>
 using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
-	int a[8]={50000,10000,5000,1000,500,100,50,10};
-    int n;
-    cin>>n;
-    for (int i=0;i<n;i++)
-    {
-     	int m;
-        cin>>m;
-        cout<<"#"<<i+1<<"\n";
-        for (int j=0;j<8;j++)
-        {
-         	cout<< m/a[j]<<" ";
-            m%=a[j];
-        }
-        cout<<"\n";
-    }
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+	int n;
+	cin >> n;
+	int money[] = { 50000,10000,5000,1000,500,100,50,10 };
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		cout << "#" << i + 1 << "\n";
+		for (int j = 0; j < 8; j++)
+		{
+			cout << x / money[j] << " ";
+			x %= money[j];
+		}
+		cout << "\n";
+	}
 }
