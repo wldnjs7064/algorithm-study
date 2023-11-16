2d04e5d7ab3ed6101100a126f41a755433a4b6ae
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-16 15:42:17 +0900

    [D2] Title: 파리 퇴치, Time: 6 ms, Memory: 13,536 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.cpp
@@ -1,39 +1,36 @@
-#include<iostream>
-
+#include <iostream>
+#include <string>
 using namespace std;
 
-int main(int argc, char** argv)
+int main()
 {
-	int n;
-    cin>>n;
-    for (int i=0;i<n;i++)
-    {
-     	int n,m;
-        cin>>n>>m;
-        int ans=0;
-        int a[15][15];
-        for (int j=0;j<n;j++)
-        {
-			for (int k=0;k<n;k++)
-                cin>>a[j][k];
-        }
-        for (int si=0;si<n-m+1;si++)
-        {
-			for (int sj=0;sj<n-m+1;sj++)
-            {
-                int cnt=0;
-                for (int b=si;b<si+m;b++)
-                {
-                    for (int c=sj;c<sj+m;c++)
-                    {
-                        cnt+=a[b][c];
-                    }
-                }
-                if(ans < cnt)
-                    ans = cnt;
-            }
-        }
-        cout<< "#"<<i+1<<" "<<ans<<"\n";
-    }
-	return 0;//정상종료시 반드시 0을 리턴해야합니다.
+	int t;
+	cin >> t;
+	int a[15][15];
+	for (int test_case = 0; test_case < t; test_case++)
+	{
+		int n, m, ans = 0;
+		cin >> n >> m;
+		for (int i = 0; i < n; i++)
+		{
+			for (int j = 0; j < n; j++)
+				cin >> a[i][j];
+
+		}
+		for (int si = 0; si < n-m+1; si++)
+		{
+			for (int sj = 0; sj < n - m + 1; sj++)
+			{
+				int sum = 0;
+				for (int i = si; i < si + m; i++)
+				{
+					for (int j = sj; j < sj + m; j++)
+						sum += a[i][j];
+				}
+				if (ans < sum)
+					ans = sum;
+			}
+		}
+		cout << "#" << test_case + 1 <<" "<< ans << "\n";
+	}
 }
