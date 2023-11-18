4c4f68ef5ddebc5737f52f8b476d61d4db209f86
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-18 10:23:07 +0900

    [Silver IV] Title: 큐 2, Time: 372 ms, Memory: 10424 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.cc
+++ 백준/Silver/18258. 큐 2/큐 2.cc
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-	ios_base::sync_with_stdio(0);
+	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-	queue<int> q;
 	int n;
 	cin >> n;
+	queue<int> q;
 	for (int i = 0; i < n; i++)
 	{
 		string str;
@@ -22,8 +22,8 @@ int main()
 		}
 		else if (str == "pop")
 		{
-			if (q.size() == 0)
-				cout << "-1" << "\n";
+			if (q.empty())
+				cout << "-1\n";
 			else
 			{
 				cout << q.front() << "\n";
@@ -31,28 +31,29 @@ int main()
 			}
 		}
 		else if (str == "size")
+		{
 			cout << q.size() << "\n";
+		}
 		else if (str == "empty")
 		{
-			if (q.size() == 0)
-				cout << "1" << "\n";
+			if (!q.empty())
+				cout << "0\n";
 			else
-				cout << "0" << "\n";
+				cout << "1\n";
 		}
 		else if (str == "front")
 		{
-			if (q.size() == 0)
-				cout << "-1" << "\n";
-			else
+			if (!q.empty())
 				cout << q.front() << "\n";
-		}
-		else if (str == "back")
-		{
-			if (q.size() == 0)
-				cout << "-1" << "\n";
 			else
-				cout << q.back() << "\n";
+				cout << "-1\n";
 		}
-
+		else
+		{
+			if (!q.empty())
+				cout << q.back() << "\n";
+			else
+				cout << "-1\n";
+		}
 	}
 }
