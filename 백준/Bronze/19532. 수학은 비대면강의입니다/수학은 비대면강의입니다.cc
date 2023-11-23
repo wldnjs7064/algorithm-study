ec53d5bdf38a81ca669c59e404d9384aa605a21f
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-23 16:34:35 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	int a, b, c, d, e, f;
+	cin >> a >> b >> c >> d >> e >> f;
+	for (int i = -999; i < 1000; i++)
+	{
+		for (int j = -999; j < 1000; j++)
+		{
+			if (a*i + b * j == c)
+			{
+				if (d*i + e * j == f)
+				{
+					cout << i << " " << j;
+					break;
+				}
+			}
+		}
+	}
+}
