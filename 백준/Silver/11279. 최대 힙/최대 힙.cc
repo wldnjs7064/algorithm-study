38f2856a630826b5cb34049d14c3736db017e66d
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-11-29 18:23:38 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+priority_queue<int, vector<int>, less<int>> q;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int n;
+	vector<int> v;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		if (x!=0)
+			q.push(x);
+		if (x == 0)
+		{
+			if (q.empty())
+				cout << "0\n";
+			else
+			{
+				cout << q.top() << "\n";
+				q.pop();
+			}
+		}
+	}
+}
