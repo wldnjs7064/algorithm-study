e4486d9df62b3fa831295f7b53fc51ce0a46a7ea
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2023-12-14 19:53:39 +0900

    [Silver III] Title: 먹을 것인가 먹힐 것인가, Time: 492 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
+++ 백준/Silver/7795. 먹을 것인가 먹힐 것인가/먹을 것인가 먹힐 것인가.cc
@@ -33,8 +33,7 @@ int main()
 		sort(va.begin(), va.end());
 		sort(vb.begin(), vb.end());
 		int cnt = 0;
-		if (va.size() > vb.size())
-		{
+		
 			for (int i = 0; i < va.size(); i++)
 			{
 				for (int j = 0; j < vb.size(); j++)
@@ -44,18 +43,8 @@ int main()
 					else break;
 				}
 			}
-		}
-		else
-		{
-			for (int i = 0; i < vb.size(); i++)
-			{
-				for (int j = 0; j < va.size(); j++)
-				{
-					if (va[j] > vb[i])
-						cnt++;
-				}
-			}
-		}
+		
+		
 		cout << cnt << "\n";
 	}
 }
