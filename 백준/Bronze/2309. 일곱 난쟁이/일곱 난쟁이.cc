9d5fc1ede1ef013981ab63189c5be5295820872b
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-01-03 13:15:15 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.cc
@@ -1,26 +1,36 @@
 #include <iostream>
+#include <string>
 #include <algorithm>
+#include <vector>
 using namespace std;
- 
-int main(){
-    int tall[10];
-    int sum = 0;
-    for (int i = 0; i < 9;i++){
-        cin >> tall[i];
-        sum += tall[i]; 
-    }
-    sort(tall, tall + 9); 
-    for (int i = 0; i < 8; i++){
-        for (int j = i + 1; j < 9;j++){
-            if(sum-(tall[i]+tall[j]) == 100){ 
-                for (int z = 0; z < 9;z++){
-                    if(z != i  && z != j){ // 2명 빼고 출력
-                        cout << tall[z] << '\n';
-                    }
-                }
-                return 0;
-            }   
-        }
-    }
-    return 0;
+
+int main()
+{
+	vector<int> v;
+	int sum = 0;
+	for (int i = 0; i < 9; i++)
+	{
+		int x;
+		cin >> x;
+		v.push_back(x);
+		sum += v[i];
+	}
+	sort(v.begin(), v.end());
+	for (int i = 0; i < 8; i++)
+	{
+		for (int j = 1; j < 9; j++)
+		{
+			if (sum - v[i] - v[j] == 100)
+			{
+				for (int z = 0; z < 9; z++)
+				{
+					if (z != i && z != j)
+						cout << v[z] << "\n";
+				}
+				return 0;
+			}
+		}
+	}
+	
+
 }
