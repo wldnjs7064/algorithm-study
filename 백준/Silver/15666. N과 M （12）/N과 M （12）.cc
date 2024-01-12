f37626e525c49086e92171b0590286881294e15f
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-01-12 22:39:58 +0900

    [Silver II] Title: N과 M (12), Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+int arr[10];
+void func(int k,int cnt)
+{
+	if (cnt == m)
+	{
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << " ";
+		cout << "\n";
+		return;
+	}
+	int tmp = -1;
+	for (int i = k; i < n; i++)
+	{
+		if (tmp != v[i])
+		{
+			arr[cnt] = v[i];
+			tmp = v[i];
+			func(i,cnt+1);
+		}
+	}
+}
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
+		int x;
+		cin >> x;
+		v.push_back(x);
+	}
+	sort(v.begin(), v.end());
+	func(0, 0);
+}
