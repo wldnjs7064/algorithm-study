b90e01a6f8ac35b20a0362c53b87be835d2e2055
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-01-14 17:31:05 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+typedef long long ll;
+
+ll a, b, c;
+ll go(ll a, ll b)
+{
+	if (b == 1) return a % c;
+	ll ret = go(a, b / 2);
+	ret = (ret*ret) % c;
+	if (b % 2) ret = (ret*a) % c;
+	return ret;
+}
+int main()
+{
+	cin >> a >> b >> c;
+	cout << go(a, b);
+}
