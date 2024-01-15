8ec0bdc7e54ec99a9d6da9c354a5e086d4b7d8ee
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-01-15 13:18:51 +0900

    [Silver II] Title: 유기농 배추, Time: 4 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -2,39 +2,33 @@
 #include <cstring>
 using namespace std;
 
-int t, m, n, k, x, y, nx, ny;
+int n, m, k, x, y;
 int a[50][50];
 bool visited[50][50];
 int dy[] = { -1,0,1,0 };
 int dx[] = { 0,1,0,-1 };
+
 void dfs(int y, int x)
 {
 	visited[y][x] = 1;
 	for (int i = 0; i < 4; i++)
 	{
-		ny = y + dy[i];
-		nx = x + dx[i];
-		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-		if (a[ny][nx] == 1 && !visited[ny][nx])
-		{
-			dfs(ny, nx);
-		}
+		int ny = y + dy[i];
+		int nx = x + dx[i];
+		if (ny < 0 || nx < 0 || nx >= m || ny >= n) continue;
+		if (!visited[ny][nx] && a[ny][nx] == 1) dfs(ny, nx);
 	}
 }
 int main()
 {
-	cin.tie(NULL);
-	cout.tie(NULL);
+	int t;
 	cin >> t;
-	for (int test = 0; test < t; test++)
+	while (t--)
 	{
-		int ret = 0; //지렁이 개수
+		int ret = 0;
 		cin >> m >> n >> k;
-
-		//초기화 부분
 		memset(a, 0, sizeof(a));
 		memset(visited, 0, sizeof(visited));
-
 		for (int i = 0; i < k; i++)
 		{
 			cin >> x >> y;
@@ -44,7 +38,7 @@ int main()
 		{
 			for (int j = 0; j < m; j++)
 			{
-				if (a[i][j] == 1 && !visited[i][j])
+				if (!visited[i][j] && a[i][j] == 1)
 				{
 					dfs(i, j);
 					ret++;
