04fd6c5be0c3d9cd0532886f69d93aaee0c7383a
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-02-16 22:03:26 +0900

    [Silver I] Title: 카드 구매하기, Time: 0 ms, Memory: 2064 KB -BaekjoonHub

---
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int p[10000];
+int d[1001];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		cin >> p[i];
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+			d[i] = max(d[i], d[i - j] + p[j]);
+	}
+	cout << d[n];
+}
