3a7bbb5ad33054ec71b2647e32ac7bd975f61218
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-02-29 15:06:02 +0900

    [Silver II] Title: 제곱수의 합, Time: 32 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int d[100001];
+int main()
+{
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++)
+		d[i] = i;
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j*j <= i; j++)
+			d[i] = min(d[i], d[i - j * j] + 1);
+	}
+	cout << d[n];
+}
