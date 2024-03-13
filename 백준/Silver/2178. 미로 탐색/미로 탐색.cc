3f44517b4b77576d49f11f594c479ac02a85b52c
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-03-13 11:31:45 +0900

    [Silver I] Title: 미로 탐색, Time: 0 ms, Memory: 2100 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.cc
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.cc
@@ -2,46 +2,43 @@
 #include <queue>
 using namespace std;
 
-int a[110][110];
-int visited[110][110];
 int n, m;
-int dy[] = { -1, 0, 1, 0 };
-int dx[] = { 0, 1, 0, -1 };
+int arr[101][101];
+int visited[101][101];
 
-int bfs(int x, int y) {
-	queue<pair<int, int>> q;
-	q.push(make_pair(x, y));
+int dy[] = { -1,0,1,0 };
+int dx[] = { 0,1,0,-1 };
+int bfs(int x, int y)
+{
+	queue<pair<int,int>> q;
+	q.push({ x,y });
 	visited[y][x] = 1;
-
-	while (!q.empty()) {
-		int x = q.front().first;
+	while (q.size())
+	{
 		int y = q.front().second;
+		int x = q.front().first;
 		q.pop();
 
-		for (int i = 0; i < 4; i++) {
-			int nx = x + dx[i];
+		for (int i = 0; i < 4; i++)
+		{
 			int ny = y + dy[i];
-
-			if (nx < 0 || ny < 0 || nx >= m || ny >= n || a[ny][nx] == 0) continue;
+			int nx = x + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || arr[ny][nx]==0) continue;
 			if (visited[ny][nx]) continue;
-
 			visited[ny][nx] = visited[y][x] + 1;
-			q.push(make_pair(nx, ny));
+			q.push({ nx,ny });
 		}
 	}
-
 	return visited[n - 1][m - 1];
 }
 
-int main() {
-	scanf("%d %d", &n, &m);
-
-	for (int i = 0; i < n; i++) {
+int main()
+{
+	cin >> n >> m;
+	for (int i = 0; i < n; i++)
+	{
 		for (int j = 0; j < m; j++)
-			scanf("%1d", &a[i][j]);
+			scanf("%1d", &arr[i][j]);
 	}
-
 	cout << bfs(0, 0);
-
-	return 0;
 }
