ee8149790ac5f297ef603d1ecc898547b561338c
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-05-13 14:34:31 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2076 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.cc
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -3,71 +3,33 @@
 #include <cstring>
 using namespace std;
 
-int n, x, y;
+int n;
 char arr[101][101];
 int visited[101][101];
 
-int dy[] = { -1,0,1,0 };
-int dx[] = { 0,1,0,-1 };
+int dx[4] = { -1,1,0,0 };
+int dy[4] = { 0,0,-1,1 };
 
-void bfs(int x, int y)
-{
-	visited[y][x] = 1;
-	char num = arr[y][x];
+void bfs(int x, int y) {
 	queue<pair<int, int>> q;
 	q.push({ x,y });
-	while (q.size())
-	{
-		int y = q.front().second;
+	visited[y][x] = 1;
+
+	while (q.size()) {
 		int x = q.front().first;
+		int y = q.front().second;
+
 		q.pop();
 
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = y + dy[i];
+		for (int i = 0; i < 4; i++) {
 			int nx = x + dx[i];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-			if (visited[ny][nx] == 0)
-			{
-				if (num == arr[ny][nx])
-				{
-					visited[ny][nx] = 1;
-					q.push({ nx,ny });
-				}
+			int ny = y + dy[i];
+
+			if (nx < 0 || ny < 0 || ny >= n || nx >= n) {
+				continue;
 			}
-		}
-	}
-}
-
-void bfs2(int x, int y)
-{
-	visited[y][x] = 1;
-	char num = arr[y][x];
-	queue<pair<int, int>> q;
-	q.push({ x,y });
-	while (q.size())
-	{
-		int y = q.front().second;
-		int x = q.front().first;
-		q.pop();
-
-		for (int i = 0; i < 4; i++)
-		{
-			int ny = y + dy[i];
-			int nx = x + dx[i];
-			if (ny < 0 || nx < 0 || ny >= n || nx >= n) continue;
-			if (visited[ny][nx] == 0)
-			{
-				if (num == 'G' || num == 'R')
-				{
-					if (arr[ny][nx] == 'G' || arr[ny][nx] == 'R')
-					{
-						visited[ny][nx] = 1;
-						q.push({ nx,ny });
-					}
-				}
-				else if (num == arr[ny][nx])
-				{
+			if (visited[ny][nx] == 0) {
+				if (arr[ny][nx] == arr[y][x]) {
 					visited[ny][nx] = 1;
 					q.push({ nx,ny });
 				}
@@ -79,40 +41,45 @@ void bfs2(int x, int y)
 int main()
 {
 	cin >> n;
-
-	memset(arr, 0, sizeof(arr));
-	memset(visited, 0, sizeof(visited));
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
+	
+	//입력받기
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
 			cin >> arr[i][j];
+		}
 	}
 
-	int ret = 0;
+	int normal = 0;
+	int result = 0;
 
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (!visited[i][j])
-			{
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (visited[i][j] == 0) {
 				bfs(j, i);
-				ret++;
+				normal++;
 			}
 		}
 	}
+
 	memset(visited, 0, sizeof(visited));
-	int greencnt = 0;
-	for (int i = 0; i < n; i++)
-	{
-		for (int j = 0; j < n; j++)
-		{
-			if (!visited[i][j])
-			{
-				bfs2(j, i);
-				greencnt++;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (arr[i][j] == 'G') {
+				arr[i][j] = 'R';
 			}
 		}
 	}
-	cout << ret << " " << greencnt;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			if (visited[i][j] == 0) {
+				bfs(j, i);
+				result++;
+			}
+		}
+	}
+	
+
+	cout << normal << " " << result;
 }
