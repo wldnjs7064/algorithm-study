586229291537c3ba280cc7be6c1c9b24a1d86cbd
jiwon Song <72726948+wldnjs7064@users.noreply.github.com>
2024-06-08 12:40:33 +0900

    [Silver III] Title: 프린터 큐, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.cc
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <vector>
 #include <queue>
 using namespace std;
 
@@ -7,39 +6,35 @@ int main()
 {
 	int t;
 	cin >> t;
-	while (t--)
-	{
-		vector<int> v;
+	while (t--) {
+		queue<pair<int, int>> q;
+		priority_queue<int> pq;
 		int n, m;
 		cin >> n >> m;
-		priority_queue<int> pq;
-		queue<pair<int, int>> q;
-		for (int i = 0; i < n; i++)
-		{
-			int imp;
-			cin >> imp;
-			q.push({ i,imp });
-			pq.push(imp);
+		for (int i = 0; i < n; i++) {
+			int a;
+			cin >> a;
+			q.push({ i,a });
+			pq.push(a);
 		}
 		int cnt = 0;
-		while (!q.empty())
-		{
-			int idx = q.front().first;
+		while (!q.empty()) {
+			int index = q.front().first;
 			int value = q.front().second;
 			q.pop();
-
-			if (pq.top() == value)
-			{
+			if (value == pq.top()) {
 				pq.pop();
 				cnt++;
-				if (m == idx)
+				if (index == m)
 				{
 					cout << cnt << "\n";
 					break;
 				}
 			}
-			else
-				q.push({ idx,value });
+			else {
+				q.push({ index,value });
+			}
 		}
+
 	}
 }
