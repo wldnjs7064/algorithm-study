196e2ac628adb8caa79d010656663584209dc519
jiwon Song <wldnjs7064@naver.com>
2024-07-01 17:34:50 +0900

    [Gold V] Title: 평범한 배낭, Time: 32 ms, Memory: 41476 KB -BaekjoonHub

---
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int dp[101][100001];
+int w[101];
+int v[101];
+
+int main()
+{
+	int n, k;
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	}
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (j >= w[i]) {
+				dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w[i]] + v[i]);
+			}
+			else {
+				dp[i][j] = dp[i - 1][j];
+			}
+		}
+	}
+	cout << dp[n][k] << "\n";
+}
